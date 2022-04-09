4ff026381c4680db4fa7d8873da56931ca0fa298
Siu Kei Tam <tam.s.k.92@gmail.com>
2022-04-09 15:01:47 +0100

    Added pre-built button component and added styling

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -1,17 +1,21 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+
+//COMPONENTS
 import SingleApplication from "./SingleApplication";
-import { getSingleApplicationFixture } from "../__fixtures__/applications.fixture";
+import { Button } from "../ui/Button/Button.jsx";
+
+//STYLES
 import styles from "../css/Applications.module.css";
 
 const Applications = () => {
-  const applications = getSingleApplicationFixture;
   
   const [data, setData] = useState(null);
+  const [currentPagination, setCurrentPagination] = useState(2);
 
   useEffect(async () => {
     try {
-      const {data} = await axios.get('http://localhost:3001/api/applications?_page=2&_limit=5')
+      const {data} = await axios.get(`http://localhost:3001/api/applications?_page=${currentPagination}&_limit=5`)
       console.log(data)
       return setData(data)
     } catch (error) {
@@ -20,11 +24,18 @@ const Applications = () => {
 
   }, [])
 
+  //Create a function to update pagination and refetch the data 
+  //When pagination goes to 20 return to 1 or disable button
+  // handleClick = () => {
+
+  // }
+
   return (
     <div className={styles.Applications}>
       {data && data.map((user, index) => {
         return <SingleApplication key={index} application={user} />
       })}
+      <Button>Load more</ Button>
     </div>
   );
 };
