752bb0caebc2a197f86746f262aef3c5e68c7e07
Siu Kei Tam <tam.s.k.92@gmail.com>
2022-04-09 18:48:36 +0100

    Refactored api logic

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -16,26 +16,32 @@ const Applications = () => {
   const [limitPerPage, setLimitPerPage] = useState(5);
   const [currentPagination, setCurrentPagination] = useState(1);
 
-  useEffect(async () => {
-    try {
-      const {data} = await axios.get(`http://localhost:3001/api/applications`)
-      console.log('total data length is:', totalData.length)
-      return setTotalData(data)
-    } catch (error) {
-      console.log(error)
+  useEffect(() => {
+    const getApplicationTotalData = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3001/api/applications");
+        return setTotalData(data)
+      } catch (error) {
+        console.log(error);
+      }
     }
+    getApplicationTotalData()
   }, [])
 
-  useEffect(async () => {
-    try {
-      const {data} = await axios.get(`http://localhost:3001/api/applications?_page=${currentPagination}&_limit=${limitPerPage}`)
-      console.log(data)
-      return setData(data)
-    } catch (error) {
-      console.log(error)
+  useEffect(() => {
+    const getApplicationData = async (currentPagination, limitPerPage) => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:3001/api/applications?_page=${currentPagination}&_limit=${limitPerPage}`
+        );
+        return setData(data)
+      } catch (error) {
+        console.log(error);
+      }
     }
+    getApplicationData(currentPagination, limitPerPage)
 
-  }, [currentPagination])
+  }, [currentPagination, limitPerPage])
 
   //Create a function to update pagination and refetch the data 
   //When pagination goes to 20 return to 1 or disable button
